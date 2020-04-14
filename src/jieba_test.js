d33ff35ac82d8acf5b59cc63f713122953ac8a7e
yuankui <yuankui64@gmail.com>
2020-04-14 19:21:07 +0800

    up

--- src/jieba_test.js
+++ src/jieba_test.js
@@ -3,6 +3,6 @@ const jieba = require('nodejieba');
 jieba.load({
     stopWordDict: '/Users/yuankui/git/grace-editor/src/search-engine/jieba_test.dict.js'
 })
-const strings = jieba.cutForSearch(`yuanxkui`, true);
+const strings = jieba.extract(`Python是世界上最好的语言`, 100);
 
-console.log(strings);
+console.log(strings.map(t => t.word));
